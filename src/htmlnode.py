a10a14a78be5d062929873b1131d390a2792ec08
rinshan77 <simon.durakovic@proton.me>
2024-07-21 15:26:18 +0200

    Added the ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -3,7 +3,7 @@ class HTMLNode():
         self.tag = tag              #String representing tag name e.g. p, a, h1
         self.value = value          #String representing value of tag e.g. text inside paragraph
         self.children = children    #List of HTMLNode objects, representing the children of this node
-        self.props = props          #Dictionary representing attributes of the tag, e.g. (<a> tag) might have {"href": "https://www.google.com"}
+        self.props = props if props is not None else {}         #Dictionary representing attributes of the tag, e.g. (<a> tag) might have {"href": "https://www.google.com"}
 
     def to_html(self):
         raise NotImplementedError("This is a placeholder for the child classes")
@@ -11,8 +11,44 @@ class HTMLNode():
     def props_to_html(self):
         string = ""
         for key, value in self.props.items():
-            string += f" {key}={value}"
+            string += f' {key}="{value}"'
         return string
+
 
     def __repr__(self):
         return f"Tag={self.tag!r} Value={self.value!r} Children={self.children!r} Props={self.props!r}"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, [], props)
+
+    def props_to_html(self):
+        if not self.props:
+            return ""
+        return " " + " ".join([f'{key}="{value}"' for key, value in self.props.items()])
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("LeafNodes MUST have a value")
+        if self.tag is None:
+            return self.value
+        if self.tag in ["img", "br"]:
+            return f"<{self.tag}{self.props_to_html()} />"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, value=None, children=None, props=None):
+        if children is None:
+            children = []
+        super().__init__(tag, value, children, props)
+    
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("ParentNodes MUST have a tag")
+        if not self.children:
+            raise ValueError("ParentNodes MUST have children")
+
+        children_html = "".join([child.to_html() for child in self.children])
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+        
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,4 +1,4 @@
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 import unittest
 
 class TestHTMLNode(unittest.TestCase):
@@ -19,3 +19,44 @@ class TestHTMLNode(unittest.TestCase):
         print(node.props_to_html())
         print(node2.props_to_html())
 
+class TestLeafNode(unittest.TestCase):
+
+    def test_to_html(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        node2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        node3 = LeafNode("img", "Yooo!", {"src": "https://www.google.com"})
+        print(node.to_html())
+        print(node2.to_html())
+        print(node3.to_html())
+
+class TestParentNode(unittest.TestCase):
+
+    def test_parent_node_with_children(self):
+        leaf1 = LeafNode(tag="span", value="Child 1", props={"class": "text-bold"})
+        leaf2 = LeafNode(tag="span", value="Child 2")
+        parent = ParentNode(tag="div", children=[leaf1, leaf2], props={"id": "parent"})
+        
+        expected_html = '<div id="parent"><span class="text-bold">Child 1</span><span>Child 2</span></div>'
+        self.assertEqual(parent.to_html(), expected_html)
+
+    def test_parent_node_without_tag(self):
+        with self.assertRaises(ValueError) as context:
+            ParentNode(tag=None, children=[LeafNode(tag="span", value="Child")]).to_html()
+        self.assertEqual(str(context.exception), "ParentNodes MUST have a tag")
+
+    def test_parent_node_without_children(self):
+        with self.assertRaises(ValueError) as context:
+            ParentNode(tag="div", children=[]).to_html()
+        self.assertEqual(str(context.exception), "ParentNodes MUST have children")
+
+    def test_recursive_parent_node(self):
+        leaf1 = LeafNode(tag="span", value="Child 1", props={"class": "text-bold"})
+        leaf2 = LeafNode(tag="span", value="Child 2")
+        inner_parent = ParentNode(tag="section", children=[leaf1, leaf2])
+        outer_parent = ParentNode(tag="div", children=[inner_parent], props={"id": "outer-parent"})
+
+        expected_html = '<div id="outer-parent"><section><span class="text-bold">Child 1</span><span>Child 2</span></section></div>'
+        self.assertEqual(outer_parent.to_html(), expected_html)
+
+if __name__ == "__main__":
+    unittest.main()
